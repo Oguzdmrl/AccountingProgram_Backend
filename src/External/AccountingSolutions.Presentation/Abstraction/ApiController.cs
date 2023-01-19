0ccdabbd9a4a322b10083cb20f23c3a3a1b150be
Oğuz Demirel <oguz@endcozum.com>
2023-01-19 11:14:19 +0300

    Add project files.

---
+++ src/External/AccountingSolutions.Presentation/Abstraction/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingSolutions.Presentation.Abstraction
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class ApiController: ControllerBase
+    {
+    }
+}
---
+++ src/External/AccountingSolutions.Presentation/Controller/TestController.cs
@@ -0,0 +1,14 @@
+﻿using AccountingSolutions.Presentation.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountingSolutions.Presentation.Controller
+{
+    public sealed class TestController : ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test başarılı");
+        }
+    }
+}
